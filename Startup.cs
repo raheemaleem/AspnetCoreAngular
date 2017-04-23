776df679db28e9e5ff2e8d609d7963178c2665c8
Raheem Aleem <raheemaleem@gmail.com>
2017-04-23 15:07:46 -0400

    Update to remove webpack dev tools

--- Startup.cs
+++ Startup.cs
@@ -41,10 +41,10 @@ namespace aspnetcore_angular_sample
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true
-                });
+                //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                //{
+                //    HotModuleReplacement = true
+                //});
             }
             else
             {
